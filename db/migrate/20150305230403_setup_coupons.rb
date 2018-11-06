353d07dc4a59670222982525a0c3b29f68cf6528
Innocent Udeogu <innocentudeh@gmail.com>
2018-11-06 09:01:56 +0100

    Update 20150305230403_setup_coupons.rb

--- db/migrate/20150305230403_setup_coupons.rb
+++ db/migrate/20150305230403_setup_coupons.rb
@@ -1,7 +1,7 @@
 class SetupCoupons < ActiveRecord::Migration[5.0]
   def change
-    drop_table :coupons
-    drop_table :coupon_redemptions
+    drop_table(:coupons, if_exists: true)
+    drop_table(:coupon_redemptions, if_exists: true)
     create_table :coupons do |t|
       t.string :code, null: false
       t.string :description, null: true
